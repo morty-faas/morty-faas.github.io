83aa40f66b28a707f0892ac4005f006906658093
thomasgouveia <gouveia.thomas@outlook.fr>
2023-04-24 14:20:15 +0200

    docs: add base layouts
    Signed-off-by: thomasgouveia <gouveia.thomas@outlook.fr>

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,23 @@
+function Logo() {
+  return (
+    <div style={{display: "flex", justifyContent: "center", alignItems:"center"}}>
+      <img style={{borderRadius: '5px', marginRight: "10px"}} width={50} src="/morty-logo-white-bg.png"></img>
+      <h1 style={{fontWeight: "bold"}}>Morty</h1>
+    </div>
+  )
+}
+
 export default {
-    logo: <span>Morty</span>,
+    logo: <Logo />,
+    primaryHue: {
+      dark: 270,
+      light: 270
+  },
     docsRepositoryBase: 'https://github.com/morty-faas/docs',
     project: {
       link: 'https://github.com/morty-faas',
     },
+    footer: {
+      text: "MIT 2023 © Morty maintainers."
+  },
   }
