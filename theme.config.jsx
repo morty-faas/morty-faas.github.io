e70339e3019f1ba81c917f3bf7c627c3ce6515c6
thomasgouveia <gouveia.thomas@outlook.fr>
2023-04-24 13:16:56 +0200

    docs: initialize Nextra
    Signed-off-by: thomasgouveia <gouveia.thomas@outlook.fr>

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span>Morty</span>,
+    docsRepositoryBase: 'https://github.com/morty-faas/docs',
+    project: {
+      link: 'https://github.com/morty-faas',
+    },
+  }
