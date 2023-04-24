757a226dd1208bd82b5eb8f10add967930631b17
LeoFVO <leofvo@proton.me>
2023-04-24 14:28:43 +0200

    docs: add contributing

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-import nextra from "nextra";
+import nextra from 'nextra';
 
 const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
-  defaultShowCopyCode: true
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  defaultShowCopyCode: true,
 });
 
 export default withNextra({
   reactStrictMode: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 });
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,23 +1,33 @@
 function Logo() {
   return (
-    <div style={{display: "flex", justifyContent: "center", alignItems:"center"}}>
-      <img style={{borderRadius: '5px', marginRight: "10px"}} width={50} src="/morty-logo-white-bg.png"></img>
-      <h1 style={{fontWeight: "bold"}}>Morty</h1>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <img
+        style={{ borderRadius: '5px', marginRight: '10px' }}
+        width={50}
+        src="/morty-logo-white-bg.png"
+      ></img>
+      <h1 style={{ fontWeight: 'bold' }}>Morty</h1>
     </div>
-  )
+  );
 }
 
 export default {
-    logo: <Logo />,
-    primaryHue: {
-      dark: 270,
-      light: 270
+  logo: <Logo />,
+  primaryHue: {
+    dark: 270,
+    light: 270,
   },
-    docsRepositoryBase: 'https://github.com/morty-faas/docs',
-    project: {
-      link: 'https://github.com/morty-faas',
-    },
-    footer: {
-      text: "MIT 2023 © Morty maintainers."
+  docsRepositoryBase: 'https://github.com/morty-faas/docs',
+  project: {
+    link: 'https://github.com/morty-faas',
   },
-  }
+  footer: {
+    text: 'MIT 2023 © Morty maintainers.',
+  },
+};
