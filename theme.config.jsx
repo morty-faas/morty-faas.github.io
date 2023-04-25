14ad85c69de61628f084b9d20d7fe84535f03d75
Malo Polese <malo.polese@gmail.com>
2023-04-25 11:23:05 +0200

    feat: seo-meta-tags

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,6 @@
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
+
 function Logo() {
   return (
     <div
@@ -40,4 +43,48 @@ export default {
   footer: {
     text: 'MIT 2023 © Morty maintainers.',
   },
+  head: () => {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+
+    const base = "https://morty-faas.github.io/";
+    const url = `${base}${asPath}`;
+    
+    let title = `${frontMatter.title}` ?? 'Morty'
+    if (asPath !== '/') {
+      title += ' - Morty'
+    }
+    const description = frontMatter.description ?? 'Morty is an open source serverless platform allowing users to deploy function as a service, without the need of managing servers.'
+    const image = frontMatter.image ?? `https://raw.githubusercontent.com/morty-faas/.github/main/assets/morty.jpg`
+
+    return <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="title" content={title}/>
+      <meta name="description" content={description}/>
+
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content={url}/>
+      <meta property="og:title" content={title}/>
+      <meta property="og:description" content={description}/>
+      <meta property="og:image" content={image}/>
+
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content={url}/>
+      <meta property="twitter:title" content={title}/>
+      <meta property="twitter:description" content={description}/>
+      <meta property="twitter:image" content={image}/>
+    </>
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath === '/') {
+      return {
+        titleTemplate: 'Morty',
+      };
+    }
+    return {
+      titleTemplate: '%s - Morty',
+    };
+  }
 };
+
