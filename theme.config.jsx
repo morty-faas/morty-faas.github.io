ad7e21cbcc96b76eadbab10ad4abc328229973cb
LeoFVO <leofvo@proton.me>
2023-04-25 11:52:51 +0200

    fix: missing parenthese

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,5 @@
-import { useRouter } from "next/router";
-import { useConfig } from "nextra-theme-docs";
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
 
 function Logo() {
   return (
@@ -24,8 +24,12 @@ export default {
   logo: <Logo />,
   banner: {
     text: (
-      <a href="https://github.com/morty-faas/morty-faas.github.io/issues/new/choose" target="_blank">
-       <strong>Morty's documentation is under construction.</strong> Give us your feedback →
+      <a
+        href="https://github.com/morty-faas/morty-faas.github.io/issues/new/choose"
+        target="_blank"
+      >
+        <strong>Morty's documentation is under construction.</strong> Give us
+        your feedback →
       </a>
     ),
   },
@@ -33,48 +37,55 @@ export default {
     dark: 270,
     light: 270,
   },
-  docsRepositoryBase: 'https://github.com/morty-faas/morty-faas.github.io/tree/main/',
+  docsRepositoryBase:
+    'https://github.com/morty-faas/morty-faas.github.io/tree/main/',
   project: {
     link: 'https://github.com/morty-faas',
   },
   editLink: {
-    text: 'Edit this page on GitHub →'
+    text: 'Edit this page on GitHub →',
   },
   footer: {
     text: 'MIT 2023 © Morty maintainers.',
   },
   head: () => {
-    const { asPath } = useRouter()
-    const { frontMatter } = useConfig()
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
 
-    const base = "https://morty-faas.github.io/";
+    const base = 'https://morty-faas.github.io/';
     const url = `${base}${asPath}`;
-    
-    let title = `${frontMatter.title}` ?? 'Morty'
+
+    let title = `${frontMatter.title}` ?? 'Morty';
     if (asPath !== '/') {
-      title += ' - Morty'
+      title += ' - Morty';
     }
-    const description = frontMatter.description ?? 'Morty is an open source serverless platform allowing users to deploy function as a service, without the need of managing servers.'
-    const image = frontMatter.image ?? `https://raw.githubusercontent.com/morty-faas/.github/main/assets/morty.jpg`
+    const description =
+      frontMatter.description ??
+      'Morty is an open source serverless platform allowing users to deploy function as a service, without the need of managing servers.';
+    const image =
+      frontMatter.image ??
+      `https://raw.githubusercontent.com/morty-faas/.github/main/assets/morty.jpg`;
 
-    return <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="title" content={title}/>
-      <meta name="theme-color" content="#321A4C" />
-      <meta name="description" content={description}/>
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="title" content={title} />
+        <meta name="theme-color" content="#321A4C" />
+        <meta name="description" content={description} />
 
-      <meta property="og:type" content="website"/>
-      <meta property="og:url" content={url}/>
-      <meta property="og:title" content={title}/>
-      <meta property="og:description" content={description}/>
-      <meta property="og:image" content={image}/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
 
-      <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content={url}/>
-      <meta property="twitter:title" content={title}/>
-      <meta property="twitter:description" content={description}/>
-      <meta property="twitter:image" content={image}/>
-    </>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
+      </>
+    );
   },
   useNextSeoProps() {
     const { asPath } = useRouter();
@@ -86,6 +97,5 @@ export default {
     return {
       titleTemplate: '%s - Morty',
     };
-  }
+  },
 };
-
