51bdadf95ad8fd8aeabd6166a30798e3910defc0
LeoFVO <leofvo@proton.me>
2023-04-25 11:35:02 +0200

    docs: add quickstart

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,15 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
   defaultShowCopyCode: true,
-  readingTime: true
+  readingTime: true,
 });
 
 export default withNextra({
   reactStrictMode: true,
   output: "export",
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   output: "export",
-  distDir: "dist"
+  distDir: "dist",
 });
